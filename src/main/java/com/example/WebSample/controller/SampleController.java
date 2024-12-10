5083649b128d715086e5ebc0e78f8d1cc4223fb3
jooyeon <jooyeon0907@naver.com>
2024-12-11 00:45:04 +0900

    HTTP 요청 파라미터 전송

--- src/main/java/com/example/WebSample/controller/SampleController.java
+++ src/main/java/com/example/WebSample/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.example.WebSample.controller;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,15 +8,49 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SampleController {
 
-    @GetMapping("/order/1")
-    public String getOrder(){
+
+
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable("orderId") String id){
         log.info("Get some order");
-        return "orderId:2, orderAmount:1000";
+        return "orderId:" + id + ", orderAmount:1000";
     }
 
-    @PostMapping("/order")
+    @DeleteMapping("/order/{orderId}")
+    public String deleteOrder(@PathVariable("orderId") String id){
+        log.info("Delete some order");
+        return "Delete orderId:" + id;
+    }
+
+    @GetMapping("/order")
+    public String getOrderWithRequestParam(
+            @RequestParam(value = "orderId", required = false, defaultValue = "defaultId") String id,
+            @RequestParam("orderAmount") Integer amount){
+        log.info("Get some order");
+        return "orderId:" + id + ", orderAmount:" + amount;
+    }
+
+    @PutMapping("/order")
     public String createOrder(){
         log.info("Create order");
         return "order created -> orderId:2, orderAmount:1000";
     }
+
+    @PostMapping("/order")
+    public String createOrder(
+            @RequestBody CreateOrderRequest createOrderRequest,
+            @RequestHeader("userAccountId") String userAccountId){
+        log.info("Create order : " + createOrderRequest +
+                ", amount : " + userAccountId);
+        return "orderId:" + createOrderRequest.getOrderId() +
+                ", orderAmount:" + createOrderRequest.getOrderAmount();
+    }
+
+    @Data
+    public static class CreateOrderRequest {
+        private String orderId;
+        private Integer orderAmount;
+    }
+
+
 }
